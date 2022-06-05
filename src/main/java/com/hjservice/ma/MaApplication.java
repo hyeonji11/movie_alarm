2532869a2b2ecdd80babbc41e285cfb834196861
hyeonji11 <hyeonji.27@daum.net>
2022-06-05 22:16:22 +0900

    #1 OAuth2 다시 구현중

--- src/main/java/com/hjservice/ma/MaApplication.java
+++ src/main/java/com/hjservice/ma/MaApplication.java
@@ -1,9 +1,12 @@
 package com.hjservice.ma;
 
+import com.hjservice.ma.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hjservice/ma/config/SecurityConfig.java
+++ src/main/java/com/hjservice/ma/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.hjservice.ma.config;
 
 import com.hjservice.ma.service.Oauth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
